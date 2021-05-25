fe4420f0eecf20f1d4c9c855ad6587815b68e110
Yeuris <yjtv@protonmail.com>
2021-05-25 01:39:49 -0400

    añade función de declarar tabla y función de leer una celda de la tabla

--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@ namespace simulador_array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] tableArr = declareTable(2, 5);
+            foreach (var item in tableArr)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(readTableCell(tableArr, 2, 4));
         }
+
+        static int[] declareTable(int rows, int columns)
+        {
+            int size = rows * columns;
+            int[] tableArr = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                tableArr[i] = i;
+            }
+            return tableArr;
+        }
+
+        static int readTableCell(int[] array, int row, int column)
+        {
+            return array[row * column];
+        }
+
+        /*static writeTableCell()
+        {
+
+        }*/
     }
 }
